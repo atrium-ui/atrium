f48606e864fc0e4fd116bb513db0fc02f97b32b2
Tim Havlicek <t.havlicek@s-v.de>
2022-12-17 17:26:06 +0100

    int(commitlint): init

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "ci", "docs", "fix", "test", "int"]],
+    "scope-empty": [2, "never"],
+  },
+};
