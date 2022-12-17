6fadfaaaa1c3a66261e59bc8c0157b027fbdcc58
Tim Havlicek <t.havlicek@s-v.de>
2022-12-17 04:50:25 +0100

    f this

---
+++ apps/library/histoire.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "histoire";
+import { HstVue } from "@histoire/plugin-vue";
+
+export default defineConfig({
+  plugins: [HstVue()],
+  setupFile: "src/histoire.setup.ts",
+  outDir: "../../public",
+  storyMatch: ["docs/**/*.story.vue"],
+  theme: {
+    title: "Atrium UI",
+    logo: {
+      square: "/src/img/logo.svg",
+      light: "/src/img/logo.svg",
+      dark: "/src/img/logo.svg",
+    },
+  },
+  tree: {
+    order: (a, b) => a.localeCompare(b),
+    groups: [
+      {
+        id: "top",
+        title: "", // No toggle
+      },
+      {
+        id: "docs",
+        title: "", // No toggle
+      },
+      {
+        id: "primitives",
+        title: "Primitives",
+      },
+      {
+        title: "Components",
+        include: (file) => !file.title.includes("Serialize"),
+      },
+    ],
+  },
+});
