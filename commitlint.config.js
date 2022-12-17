f6b25c148a2e528cfa3bd98b7bdfbe281b63a558
Tim Havlicek <t.havlicek@s-v.de>
2022-12-17 23:36:52 +0100

    int: commit scope optional

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "type-enum": [2, "always", ["feat", "ci", "docs", "fix", "test", "int"]],
-    "scope-empty": [2, "never"],
+    // "scope-empty": [2, "never"],
   },
 };
