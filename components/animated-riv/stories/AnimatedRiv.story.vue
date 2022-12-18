68c2e243f80f6535d6bb55d09d74e3e39c049e73
Tim Havlicek <t.havlicek@s-v.de>
2022-12-18 04:43:38 +0100

    int: migrate some experimental compoennts

---
+++ components/animated-riv/stories/AnimatedRiv.story.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import "../src/index";
+
+export default defineComponent({
+  data() {
+    return {
+      progress: 0,
+    };
+  },
+});
+</script>
+
+<template>
+  <Story>
+    <template #controls> Progress:<input v-model.number="progress" /> </template>
+
+    <animated-riv :loaded="progress" src="example.riv"></animated-riv>
+  </Story>
+</template>
+
+<style scoped>
+animated-riv {
+  background: black;
+}
+</style>
