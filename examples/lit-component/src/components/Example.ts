0baaf96181dca59a10c7c5c0630959f9ef9d0523
Tim Havlicek <t.havlicek@s-v.de>
2022-12-18 04:39:00 +0100

    int: example component

---
+++ examples/lit-component/src/components/Example.ts
@@ -0,0 +1,26 @@
+import { html, css, HTMLTemplateResult, LitElement } from "lit";
+import { customElement, property, query } from "lit/decorators.js";
+
+@customElement("aui-example")
+export class Example extends LitElement {
+  public static get styles() {
+    return [
+      css`
+        :host {
+          display: block;
+        }
+      `,
+    ];
+  }
+
+  @property({ type: Boolean, reflect: true })
+  public prop?: boolean;
+
+  protected render(): HTMLTemplateResult {
+    return html`
+      <div class="container">
+        <slot></slot>
+      </div>
+    `;
+  }
+}
---
+++ examples/lit-component/stories/Example.story.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import "@atrium-ui/mono/examples/lit-component";
+</script>
+
+<template>
+  <Story group="primitives">
+    <aui-example prop="example"></aui-example>
+  </Story>
+</template>
+
+<style scoped>
+aui-example {
+  display: inline;
+}
+</style>
