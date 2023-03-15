570ae94ec6f6b5ec95b3e3aeac9605d00ba5143b
Tim Havlicek <t.havlicek@s-v.de>
2023-03-15 12:35:37 +0100

    remove commitlint

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [2, "always", ["feat", "ci", "docs", "fix", "test", "int"]],
-    // "scope-empty": [2, "never"],
-  },
-};
