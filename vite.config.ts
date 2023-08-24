64d6bb27bc9ff2a49e7aeabe01f74f3e582a06e8
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-08-24 13:36:26 +0200

    copy pastable components

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vitest/config";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+  root: "src",
+  plugins: [solidPlugin()],
   test: {
     environment: "happy-dom",
   },
