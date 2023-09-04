eb111a31c42d3680f623f988aabd5cd5af8b5ae0
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-09-04 12:26:25 +0200

    feat: logo

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
-import lit from "@astrojs/lit";
-import tailwind from "@astrojs/tailwind";
+import lit from '@astrojs/lit';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/mono",
+  base: '/mono',
   experimental: {
     viewTransitions: true,
   },
@@ -15,36 +15,39 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     starlight({
-      title: "Atrium",
-      customCss: ["./src/styles/custom.css"],
+      title: 'Atrium',
+      customCss: ['./src/styles/custom.css'],
+      logo: {
+        src: './src/assets/atrium.png',
+      },
       social: {
-        gitlab: "https://gitlab.s-v.de/sv-components/mono",
+        gitlab: 'https://gitlab.s-v.de/sv-components/mono',
       },
       sidebar: [
         {
-          label: "Installation",
-          link: "/guides/installation",
+          label: 'Installation',
+          link: '/guides/installation',
         },
         {
-          label: "Primitives",
-          link: "/guides/primitives",
+          label: 'Primitives',
+          link: '/guides/primitives',
         },
         {
-          label: "Development",
-          link: "/guides/development",
+          label: 'Development',
+          link: '/guides/development',
         },
         {
-          label: "Components",
-          autogenerate: { directory: "components" },
+          label: 'Components',
+          autogenerate: { directory: 'components' },
         },
         {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
         },
       ],
     }),
   ],
 
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: { service: { entrypoint: "astro/assets/services/sharp" } },
+  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
 });
