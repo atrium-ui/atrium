6596ddb995bc35f31389bdab95c7bb839e480a81
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-09-18 16:05:20 +0200

    custom build script

---
+++ cli/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+directories=$(find ./packages -type f -name "package.json"  -exec dirname {} \;)
+
+run_build() {
+  dir="$1"
+  cd "$dir" || return
+  if [ -f "package.json" ]; then
+    npm run build
+  fi
+  cd - >/dev/null || return
+}
+
+export -f run_build
+
+echo "$directories" | parallel -j4 -k run_build {}
