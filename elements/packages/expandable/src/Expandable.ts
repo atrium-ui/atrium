119caac086234cf6e064546d7a435107f7c32a98
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-11-21 19:13:57 +0100

    fixed?

--- elements/packages/expandable/src/Expandable.ts
+++ elements/packages/expandable/src/Expandable.ts
@@ -1,5 +1,5 @@
-import { HTMLTemplateResult, LitElement, css, html } from "lit";
-import { property } from "lit/decorators.js";
+import { HTMLTemplateResult, LitElement, css, html } from 'lit';
+import { property } from 'lit/decorators.js';
 
 export class Expandable extends LitElement {
   public static get styles() {
@@ -43,7 +43,7 @@ export class Expandable extends LitElement {
   }
 
   @property({ type: Boolean, reflect: true }) public opened = false;
-  @property({ type: String, reflect: true }) public direction = "down";
+  @property({ type: String, reflect: true }) public direction = 'down';
 
   private _id = `expandable_${Math.floor(Math.random() * 100000)}`;
 
@@ -91,19 +91,21 @@ export class Expandable extends LitElement {
 
   protected render(): HTMLTemplateResult {
     return html`
-      ${this.direction === "down" ? this.renderButton() : undefined}
-      <div class="container" id="${this._id}" aria-hidden=${!this.opened && "true"}>
+      ${this.direction === 'down' ? this.renderButton() : undefined}
+      <div class="container" id="${this._id}" aria-hidden=${!this.opened && 'true'}>
         <slot class="content"></slot>
       </div>
-      ${this.direction === "up" ? this.renderButton() : undefined}
+      ${this.direction === 'up' ? this.renderButton() : undefined}
     `;
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    "a-expandable": Expandable;
+    'a-expandable': Expandable;
   }
 }
 
-customElements.define("a-expandable", Expandable);
+if (!customElements.get('a-expandable')) {
+  customElements.define('a-expandable', Expandable);
+}
