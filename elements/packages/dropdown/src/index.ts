6945662c512c7018bfac029a4cba07fc6fab3244
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-11-21 19:36:52 +0100

    fixed?

--- elements/packages/dropdown/src/index.ts
+++ elements/packages/dropdown/src/index.ts
@@ -1,4 +1,4 @@
-import '@atrium-ui/elements/expandable';
+// import '@atrium-ui/elements/expandable';
 
 export { Dropdown } from './components/dropdown/Dropdown';
 export { OptionElement } from './components/dropdown/Option';
