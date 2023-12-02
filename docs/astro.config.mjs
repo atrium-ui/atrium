7475650b3c476aaf50d3fc2755a04b619b266cfe
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-12-02 02:10:15 +0100

    stupid

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,8 +6,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	// base: '/sv-frontend-library/mono',
-	// site: 'https://sv.pages.s-v.de',
+	base: '/sv-frontend-library/mono',
+	site: 'https://sv.pages.s-v.de',
 	publicDir: 'static',
 	integrations: [
 		lit(),
