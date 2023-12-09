a7e2a383d4cff90d5a994ebc3d1605e0556e3421
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-12-09 20:50:26 +0100

    feat: notifi publish

--- scripts/generate-notify.js
+++ scripts/generate-notify.js
@@ -1,7 +1,30 @@
-function generateMesssage() {
-	const message = 'X';
+import fs from 'fs';
 
-	return message;
+const publishOutputLog = '.task/publish.txt';
+
+function parseChangelog() {
+	// recursvilery search for "CHANGELOG" files
+}
+
+function generateMesssage() {
+	if (!fs.existsSync(publishOutputLog)) {
+		throw new Error('No publish output log found');
+	}
+
+	const publishOutput = fs.readFileSync(publishOutputLog, 'utf8');
+
+	const publishedPackages = publishOutput.matchAll(
+		/info Publishing "([@a-zA-Z\/-0-9]+)" at "([-.0-9a-z]+)"/g
+	);
+
+	let message = '\n';
+
+	for (const [_, pkg, version] of publishedPackages) {
+		parseChangelog(pkg, version);
+		message += `Published ${pkg}@${version}\n`;
+	}
+
+	return `${message}\n`;
 }
 
 console.log(generateMesssage());
