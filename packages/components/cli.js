c677129e1d20bfd859f414e01f08046a562ad7c0
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-01-13 22:36:33 +0100

    cli base dir

--- packages/components/cli.js
+++ packages/components/cli.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-require('child_process').execSync(`./dist/use_${process.platform}_${process.arch}`, {
+require('child_process').execSync(`${__dirname}/dist/use_${process.platform}_${process.arch}`, {
 	stdio: 'inherit',
 });
