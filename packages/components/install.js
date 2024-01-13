da4bf022b10b688b8c4fd3440808f87b95574eba
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-01-13 22:22:45 +0100

    maybe fix install script

--- packages/components/install.js
+++ packages/components/install.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 const fs = require('fs');
 try {
-	fs.renameSync(`./dist/use_${process.platform}_${process.arch}`, './bin/use', { recursive: true });
+	fs.existsSync('./bin') || fs.mkdirSync('./bin');
+	fs.renameSync(`./dist/use_${process.platform}_${process.arch}`, './bin/use');
 	fs.rmdirSync('./dist', { recursive: true, force: true });
 } catch (err) {
 	console.error('postinstall script failed.');
