8fdbdd0f9ede39d20e9f917f15fe9f246cbd6130
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-01-14 22:13:57 +0100

    Improve cli test

--- packages/components/test/basic.spec.ts
+++ packages/components/test/basic.spec.ts
@@ -1,8 +1,15 @@
 import { describe, expect, it } from 'bun:test';
+import fs from 'fs';
 
 describe('cli', () => {
 	it('use button template', async () => {
 		const out = await import('../cli.js').then((m) => m.default);
 		expect(out.status).toBe(0);
+
+		const dir = fs.readdirSync('./src/components/ui/');
+		expect(dir).toContain('button.tsx');
+
+		// cleanup
+		fs.rmSync('./src', { force: true, recursive: true });
 	});
 });
