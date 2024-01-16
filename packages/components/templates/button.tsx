3023ff1bdad5309c9083e09d95e599ee75e12dc8
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-01-16 17:48:33 +0100

    Button Links and Image Text layout

--- packages/components/templates/button.tsx
+++ packages/components/templates/button.tsx
@@ -1,17 +1,16 @@
-const variants = {
+const buttonVariants = {
 	default: [
 		'bg-[var(--button-color,#C09278)]',
 		'filter hover:brightness-110 active:brightness-90 active:contrast-125',
-		'border border-[var(--button-color,#C09278)]',
+		'border border-width-[var(--button-border-width,1px)] border-[var(--button-color,#C09278)]',
 		'disabled:opacity-80 disabled:text-white disabled:filter-none',
 	].join(' '),
 	outline: [
 		'bg-transparent hover:bg-[rgba(150,150,150,0.1)]',
 		'filter hover:brightness-110 active:brightness-90 active:contrast-125',
-		'border border-[var(--button-color,#C09278)]',
+		'border border-width-[var(--button-border-width,1px)] border-[var(--button-color,#C09278)]',
 		'disabled:opacity-80 disabled:text-white disabled:filter-none',
 	].join(' '),
-	_ghost: 'bg-transparent p-2 flex items-center gap-2 hover:text-[#C09278]',
 	ghost: [
 		'bg-transparent active:bg-[rgba(150,150,150,0.1)]',
 		'filter hover:brightness-110',
@@ -19,36 +18,85 @@ const variants = {
 	].join(' '),
 };
 
-interface Props {
-	type?: 'button' | 'submit';
-	disabled?: boolean;
-	children?: JSX.Element | string;
-	variant?: keyof typeof variants;
-	slot?: string;
-	class?: string;
-	onClick?: (e: PointerEvent) => void;
-}
-
-export function Button(props: Props, context) {
+export function Button(
+	props: {
+		type?: 'button' | 'submit';
+		disabled?: boolean;
+		children?: JSX.Element | string;
+		variant?: keyof typeof buttonVariants;
+		slot?: string;
+		class?: string;
+		onClick?: (e: PointerEvent) => void;
+	},
+	context
+) {
 	const slots = {
 		default: () =>
 			props.children ? props.children : context?.slots?.default ? context?.slots.default() : null,
 	};
+
+	const classes = [
+		'flex gap-2 items-center cursor-pointer disabled:cursor-not-allowed',
+		'px-5 py-1 rounded-lg transition-all active:transition-none',
+		buttonVariants[props.variant ?? 'default'],
+		props.class,
+	].join(' ');
 
 	return (
 		<button
 			type={props.type || 'button'}
 			slot={props.slot}
 			disabled={props.disabled || undefined}
-			class={[
-				'flex gap-2 items-center cursor-pointer disabled:cursor-not-allowed',
-				'px-5 py-1 rounded-lg transition-all active:transition-none',
-				variants[props.variant ?? 'default'],
-				props.class,
-			].join(' ')}
+			class={classes}
 			onClick={props.onClick}
 		>
 			<slots.default />
 		</button>
+	);
+}
+
+const linkVariants = {
+	default: ['filter hover:brightness-110 active:brightness-90 active:contrast-125'].join(' '),
+	button: [
+		'text-white no-underline px-5 py-1 rounded-lg',
+		'bg-[var(--button-color,#C09278)]',
+		'filter hover:brightness-110 active:brightness-90 active:contrast-125',
+		'border border-[var(--button-color,#C09278)]',
+	].join(' '),
+	'button-outline': [
+		'text-white no-underline px-5 py-1 rounded-lg',
+		'bg-transparent hover:bg-[rgba(150,150,150,0.1)]',
+		'filter hover:brightness-110 active:brightness-90 active:contrast-125',
+		'border border-width-[var(--button-border-width,1px)] border-[var(--button-color,#C09278)]',
+		'disabled:opacity-80 disabled:text-white disabled:filter-none',
+	].join(' '),
+};
+
+export function Link(
+	props: {
+		children?: JSX.Element | string;
+		variant?: keyof typeof linkVariants;
+		class?: string;
+		href: string;
+		target?: string;
+	},
+	context
+) {
+	const slots = {
+		default: () =>
+			props.children ? props.children : context?.slots?.default ? context?.slots.default() : null,
+	};
+
+	const classes = [
+		'inline cursor-pointer',
+		'transition-all active:transition-none',
+		linkVariants[props.variant ?? 'default'],
+		props.class,
+	].join(' ');
+
+	return (
+		<a class={classes} href={props.href} target={props.target}>
+			<slots.default />
+		</a>
 	);
 }
--- packages/components/templates/imagetext.tsx
+++ packages/components/templates/imagetext.tsx
@@ -5,12 +5,9 @@ interface Props {}
 
 export function ImageText(props: Props) {
 	return (
-		<div class="grid grid-cols-[auto_1fr] gap-8">
-			<div>
-				<img src="https://picsum.photos/300/300" width="300" height="300" alt="" load="async" />
-			</div>
-			<div>
-				<h1>Image Text</h1>
+		<div class="grid grid-cols-10 xl:grid-cols-8 grid-gap !gap-y-10">
+			<div class="col-span-10 md:col-span-6 lg:col-span-7 xl:col-span-5">
+				<h3 class="text-4xl pb-6">Image Text</h3>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas asperiores, quisquam,
 					voluptates, voluptatum quibusdam accusantium quos voluptatem quia doloribus quidem
@@ -20,6 +17,22 @@ export function ImageText(props: Props) {
 				<div class="pt-10">
 					<Button>Button</Button>
 				</div>
+			</div>
+
+			<div class="col-start-6 col-span-6 md:col-span-4 lg:col-span-3 md:pl-10">
+				<figure>
+					<img
+						class="w-full h-auto"
+						src="https://picsum.photos/300/300"
+						width="300"
+						height="300"
+						alt="alt"
+						load="async"
+					/>
+					<figcaption class="text-sm">
+						<p>Caption</p>
+					</figcaption>
+				</figure>
 			</div>
 		</div>
 	);
