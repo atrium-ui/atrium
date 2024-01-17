b59b811a1e5e88063b640959107b1440b0e45c54
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-01-17 11:52:46 +0100

    Fix versioning messages

--- scripts/change-commit.cjs
+++ scripts/change-commit.cjs
@@ -3,7 +3,7 @@ module.exports = {
 		return `version: ${changeset.summary}`;
 	},
 	async getVersionMessage(releasePlan) {
-		return `version: publishing ${releasePlan.releases.length} packages
+		return `publish: ${releasePlan.releases.length} packages
 
 ${releasePlan.releases.map((release) => `- ${release.name}@${release.newVersion}`).join('\n')}
 `;
