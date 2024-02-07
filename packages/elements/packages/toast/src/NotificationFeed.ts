fdaf2c4903b400b9a0dc451ed9ddf5d885079679
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-02-07 18:21:23 +0100

    copy notifications element

---
+++ packages/elements/packages/toast/src/NotificationFeed.ts
@@ -0,0 +1,42 @@
+import { LitElement, css, html } from 'lit';
+
+let instance: NotificationFeed | null = null;
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'notification-feed': NotificationFeed;
+	}
+}
+
+export default class NotificationFeed extends LitElement {
+	static get styles() {
+		return css`
+      :host {
+        display: block;
+      }
+    `;
+	}
+
+	constructor() {
+		super();
+		// eslint-disable-next-line @typescript-eslint/no-this-alias
+		instance = this;
+	}
+
+	protected render() {
+		return html`
+      <div>
+        <slot></slot>
+      </div>
+    `;
+	}
+
+	static getInstance() {
+		if (!instance) {
+			instance = new NotificationFeed();
+		}
+		return instance;
+	}
+}
+
+customElements.define('notification-feed', NotificationFeed);
