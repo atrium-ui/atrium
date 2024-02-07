90cd7028d1c88e60e649b5c7c04cc73dfae5dc6f
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-02-07 16:18:36 +0100

    Toast init

---
+++ packages/elements/packages/toast/src/Toast.ts
@@ -0,0 +1,107 @@
+import { HTMLTemplateResult, LitElement, css, html } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'a-toast': Toast;
+	}
+}
+
+/**
+ * # a-blur
+ *
+ * - A Blur is a wrapper element which can show and hide its children.
+ * - It will automatically "blur" (hide) the content, when clicked outside of its children.
+ * - Optionally, when the content is shown, scrolling will be locked.
+ *
+ * ## Props
+ *
+ * @attribute enabled (default: false) - Whether the blur is enabled or not.
+ * @attribute scrollLock (default: false) - Whether the blur should lock scrolling when shown.
+ *
+ * @example
+ * ```html
+ * <a-blur>
+ * 	<div>
+ * 		<h1>Modal</h1>
+ * 		<p>Click outside of this modal to close it.</p>
+ * 	</div>
+ * </a-blur>
+ * ```
+ *
+ * @see https://sv.pages.s-v.de/sv-frontend-library/mono/elements/a-blur/
+ */
+@customElement('a-toast')
+export class Toast extends LitElement {
+	public static styles = css`
+    :host {
+      display: block;
+      transition-property: all;
+      pointer-events: none;
+    }
+
+    :host([enabled]) {
+      pointer-events: all;
+    }
+  `;
+
+	/**
+	 * ### enabled
+	 * Whether the blur is enabled or not.
+	 * @defaultValue false
+	 */
+	@property({ type: Boolean, reflect: true })
+	public enabled = false;
+
+	/**
+	 * ### scrollLock
+	 * Whether the blur should lock scrolling when shown.
+	 * @defaultValue false
+	 */
+	@property({ type: Boolean, reflect: true })
+	public scrollLock = false;
+
+	@query('slot')
+	slot;
+
+	protected updated(): void {
+		if (this.enabled) {
+			this.setAttribute('aria-hidden', 'false');
+		} else {
+			this.setAttribute('aria-hidden', 'true');
+		}
+	}
+
+	shouldBlur(e: MouseEvent) {
+		if (e.target === this && this.contains(e.target as HTMLElement)) {
+			return this.enabled;
+		}
+		return false;
+	}
+
+	handleClick = (e: MouseEvent) => {
+		if (this.shouldBlur(e)) {
+			this.dispatchEvent(new Event('blur'));
+			this.enabled = false;
+		}
+	};
+
+	handleCloseEvent = (e: Event) => {
+		this.enabled = false;
+	};
+
+	connectedCallback(): void {
+		super.connectedCallback();
+		this.addEventListener('mousedown', this.handleClick);
+		this.addEventListener('close', this.handleCloseEvent, { capture: true });
+	}
+
+	disconnectedCallback(): void {
+		this.removeEventListener('mousedown', this.handleClick);
+		this.removeEventListener('close', this.handleCloseEvent, { capture: true });
+	}
+
+	render(): HTMLTemplateResult {
+		return html`<slot></slot>`;
+	}
+}
