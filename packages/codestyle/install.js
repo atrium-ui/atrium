13187c271b633a404649ad0c5d81066a60653f91
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-02-15 14:38:16 +0100

    codestyle install script

---
+++ packages/codestyle/install.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+/// <reference types="node" />
+
+import fs from "node:fs";
+import path from "node:path";
+
+const root_package = process.env.npm_package_json;
+
+if(!root_package) {
+  console.error("This script must be run with npm");
+  process.exit(1);
+}
+
+const root_path = path.dirname(root_package);
+
+const editorconfig_path = path.join(root_path, ".editorconfig");
+
+const configs = {
+  'editorconfig': path.resolve("./.editorconfig")
+}
+
+// TODO: Diff the files and prompt the user to overwrite
+if(!fs.existsSync(editorconfig_path)) {
+  fs.writeFileSync(editorconfig_path, fs.readFileSync(configs.editorconfig));
+}
+
+process.exit(0);
