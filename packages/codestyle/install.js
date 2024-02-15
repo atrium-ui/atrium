3d65735ed60dfd60d7064e4390ae2a528fe1f560
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-02-15 19:45:31 +0100

    Fix install script

--- packages/codestyle/install.js
+++ packages/codestyle/install.js
@@ -4,27 +4,43 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-let root_package = process.env.npm_package_json || process.env.npm_config_dir /* pnpm */;
-
 const DEBUG = process.env.NODE_DEBUG === 'true';
 
-if (!root_package) {
+function getRootPackagePaath() {
+	// pnpm
+	if (process.env.npm_config_dir) {
+		return process.env.npm_config_dir;
+	}
+
+	// npm, yarn and bun
+	if (process.env.npm_package_json) {
+		return path.dirname(process.env.npm_package_json);
+	}
+
 	// Fallback to parent package
 	if (DEBUG)
 		console.debug(
 			'root_package could not be resolved from env, falling back to parent package'
 		);
 
-	root_package = path.resolve('../../package.json');
+	const root_package = path.resolve('../../');
+
 	if (DEBUG) console.debug('root_package:', root_package);
 
-	if (!fs.existsSync(root_package)) {
-		console.error('This script must be run with npm');
-		process.exit(1);
+	if (fs.existsSync(root_package)) {
+		return root_package;
 	}
+
+	return null;
 }
 
-const root_path = path.dirname(root_package);
+const root_path = getRootPackagePaath();
+
+if (!root_path) {
+	console.error('Could not resolve root package. Ignore.');
+	process.exit(0);
+}
+
 const editorconfig_path = path.join(root_path, '.editorconfig');
 
 const configs = {
