2436cdb8b076fedf411dea8adaa21c573449a0bf
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-03-11 13:14:29 +0100

    Flatten commitlint config

---
+++ packages/codestyle/commitlint.js
@@ -0,0 +1,98 @@
+import { RuleConfigSeverity } from "@commitlint/types";
+
+// biome-ignore lint/style/noDefaultExport: <explanation>
+export default {
+  parserPreset: "conventional-changelog-conventionalcommits",
+  rules: {
+    "body-leading-blank": [RuleConfigSeverity.Warning, "always"],
+    "body-max-line-length": [RuleConfigSeverity.Error, "always", 100],
+    "footer-leading-blank": [RuleConfigSeverity.Warning, "always"],
+    "footer-max-line-length": [RuleConfigSeverity.Error, "always", 100],
+    "header-max-length": [RuleConfigSeverity.Error, "always", 100],
+    "header-trim": [RuleConfigSeverity.Error, "always"],
+    "subject-case": [
+      RuleConfigSeverity.Error,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+    "subject-empty": [RuleConfigSeverity.Error, "never"],
+    "subject-full-stop": [RuleConfigSeverity.Error, "never", "."],
+    "type-case": [RuleConfigSeverity.Error, "always", "lower-case"],
+    "type-empty": [RuleConfigSeverity.Error, "never"],
+    "type-enum": [
+      RuleConfigSeverity.Error,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+
+    "scope-empty": [2, "never"],
+    "scope-case": [0, "always", ["upper-case", "lower-case"]],
+    "scope-enum": [2, "always", ["frontend", "cms", "integration", "global"]],
+  },
+  prompt: {
+    questions: {
+      type: {
+        description: "Select the type of change that you're committing",
+        enum: {
+          feat: {
+            description: "A new feature",
+            title: "Features",
+            emoji: "✨",
+          },
+          fix: {
+            description: "A bug fix",
+            title: "Bug Fixes",
+            emoji: "🐛",
+          },
+          docs: {
+            description: "Documentation only changes",
+            title: "Documentation",
+            emoji: "📚",
+          },
+          refactor: {
+            description: "A code change that neither fixes a bug nor adds a feature",
+            title: "Code Refactoring",
+            emoji: "📦",
+          },
+          test: {
+            description: "Adding missing tests or correcting existing tests",
+            title: "Tests",
+            emoji: "🚨",
+          },
+          ci: {
+            description:
+              "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)",
+            title: "Continuous Integrations",
+            emoji: "⚙️",
+          },
+          chore: {
+            description: "Other changes that don't modify src or test files",
+            title: "Chores",
+            emoji: "♻️",
+          },
+        },
+      },
+      scope: {
+        description: "What is the scope of this change (e.g. component or file name)",
+      },
+      subject: {
+        description: "Write a short, imperative tense description of the change",
+      },
+      issues: {
+        description: 'Add issue references (e.g. "fix #123", "re #123".)',
+      },
+    },
+  },
+};
