bfcb52f42044ba0a2c107b22083621fe11baa1da
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-03-29 18:18:23 +0100

    Codestyle postinstall script

--- packages/codestyle/test/cli.spec.ts
+++ packages/codestyle/test/cli.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from "bun:test";
 import { $ } from "bun";
 
 test("prepare script", async () => {
-  const out = await $`npm run prepare`;
+  const out = await $`npm run postinstall`;
   expect(out.exitCode).toBe(0);
 });
