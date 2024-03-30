c3b91871a9f53fbb92109916c85f7bb73d0fee67
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-03-30 16:17:43 +0100

    Fix check

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,8 +1,17 @@
-import starlightPlugin from '@astrojs/starlight-tailwind';
+import starlightPlugin from "@astrojs/starlight-tailwind";
 
 // Generated color palettes
-const accent = { 200: '#e3c492', 600: '#8d6300', 900: '#442d00', 950: '#322000' };
-const gray = { 100: '#f6f6f7', 200: '#ededef', 300: '#c1c2c4', 400: '#8b8b8f', 500: '#57585b', 700: '#37383b', 800: '#26262a', 900: '#18181a' };
+const accent = { 200: "#e3c492", 600: "#8d6300", 900: "#442d00", 950: "#322000" };
+const gray = {
+  100: "#f6f6f7",
+  200: "#ededef",
+  300: "#c1c2c4",
+  400: "#8b8b8f",
+  500: "#57585b",
+  700: "#37383b",
+  800: "#26262a",
+  900: "#18181a",
+};
 
 /** @type {import('tailwindcss').Config} */
 export default {
