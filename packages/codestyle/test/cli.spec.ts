6dadb7c826cca811ec9b4a1306748e75948526d8
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-04-08 09:40:03 +0200

    Add test cleanup

--- packages/codestyle/test/cli.spec.ts
+++ packages/codestyle/test/cli.spec.ts
@@ -1,6 +1,14 @@
 import { $ } from "bun";
-import { expect, test } from "bun:test";
+import { afterEach, expect, test } from "bun:test";
 import fs from "node:fs";
+import path from "node:path";
+
+const testPackagePath = "test/pkg";
+const testPackageJsonPath = path.resolve(`${testPackagePath}/package.json`);
+
+afterEach(async () => {
+  await $`git reset --hard ${testPackagePath}`;
+});
 
 test("prepare script", async () => {
   const out = await $`npm run postinstall`;
@@ -8,20 +16,20 @@ test("prepare script", async () => {
 });
 
 test("test prettier", async () => {
-  const out = await $`cd test/pkg && bun x @sv/codestyle prettier`;
+  const out = await $`cd ${testPackagePath} && bun x @sv/codestyle prettier`;
   expect(out.exitCode).toBe(0);
-  const pkg = fs.readFileSync("test/pkg/package.json");
+  const pkg = fs.readFileSync(testPackageJsonPath);
   const json = JSON.parse(pkg.toString());
   // @ts-ignore
   expect(json.prettier).toBeString();
 });
 
 test("test biome", async () => {
-  const out = await $`cd test/pkg && bun x @sv/codestyle biome`;
+  const out = await $`cd ${testPackagePath} && bun x @sv/codestyle biome`;
   expect(out.exitCode).toBe(0);
-  expect(fs.existsSync("test/biome.json")).toBeTrue();
+  expect(fs.existsSync(`${testPackagePath}/biome.json`)).toBeTrue();
 
-  const pkg = fs.readFileSync("test/pkg/package.json");
+  const pkg = fs.readFileSync(testPackageJsonPath);
   const json = JSON.parse(pkg.toString());
   // @ts-ignore
   expect(json.devDependencies["@biomejs/biome"]).toBeString();
