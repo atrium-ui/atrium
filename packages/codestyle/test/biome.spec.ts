36931b51a2e653d2bbbb92c1807650e43df27d12
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-04-13 06:06:34 +0200

    Remove bad test

--- packages/codestyle/test/biome.spec.ts
+++ packages/codestyle/test/biome.spec.ts
@@ -1,8 +1,9 @@
 import { $ } from "bun";
 import { expect, test } from "bun:test";
 
-test("valid biome configuration", async () => {
-  // check runs in root of this repo, since it extends this package.
-  const out = await $`bun x biome check .`;
-  expect(out.exitCode).toBe(0);
-});
+// bad test
+// test("valid biome configuration", async () => {
+//   // check runs in root of this repo, since it extends this package.
+//   const out = await $`bun x biome check .`;
+//   expect(out.exitCode).toBe(0);
+// });
