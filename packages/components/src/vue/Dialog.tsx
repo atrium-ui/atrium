a9b50dcfdd5f2345a31251fe04f4717d58488be2
Tim Havlicek <t.havlicek@s-v.de>
2024-04-15 01:25:41 +0200

    Implement focus management in a-blur

--- packages/components/src/vue/Dialog.tsx
+++ packages/components/src/vue/Dialog.tsx
@@ -1,20 +1,53 @@
 /* @jsxImportSource vue */
 
+import { defineComponent, ref } from "vue";
 import "@sv/elements/blur";
+import { Button } from "./Button";
 
-interface Props {
-  enabled: boolean;
-}
+export const Dialog = defineComponent((_, { slots }) => {
+  const open = ref(false);
 
-export function Dialog(props: Props, { slots }) {
-  return (
-    <a-blur
-      enabled={props.enabled || undefined}
-      class="group/blur fixed top-0 left-0 z-50 block h-full w-full transition-all [&[enabled]]:bg-[#33333333] [&[enabled]]:backdrop-blur-md"
-    >
-      <div class="-translate-x-1/2 -translate-y-1/2 fixed top-1/2 left-1/2 min-w-[400px] scale-95 rounded-lg border border-zinc-700 bg-zinc-800 px-8 py-8 opacity-0 transition-all group-[&[enabled]]/blur:block group-[&[enabled]]/blur:scale-100 group-[&[enabled]]/blur:opacity-100">
-        {slots.default?.()}
-      </div>
-    </a-blur>
+  return () => (
+    <div>
+      <Button
+        onClick={() => {
+          open.value = true;
+        }}
+      >
+        Open
+      </Button>
+
+      {/* should eventually use native <dialog /> */}
+      <a-blur
+        enabled={open.value || undefined}
+        class={[
+          "group/dialog fixed top-0 left-0 z-50 block h-full w-full transition-all",
+          "[&[enabled]]:bg-[#33333333] [&[enabled]]:backdrop-blur-md",
+        ]}
+        onBlur={() => {
+          open.value = false;
+        }}
+      >
+        <div
+          class={[
+            "rounded-lg border border-zinc-700 bg-zinc-800 px-8 py-8 opacity-0 transition-all",
+            "-translate-x-1/2 -translate-y-1/2 fixed top-1/2 left-1/2 min-w-[400px]",
+            "scale-95 group-[&[enabled]]/dialog:block group-[&[enabled]]/dialog:scale-100 group-[&[enabled]]/dialog:opacity-100",
+          ]}
+        >
+          {slots.default?.()}
+
+          <div class="mt-4">
+            <Button
+              onClick={() => {
+                open.value = false;
+              }}
+            >
+              Close
+            </Button>
+          </div>
+        </div>
+      </a-blur>
+    </div>
   );
-}
+});
---
+++ packages/components/src/vue/Lightbox.tsx
@@ -0,0 +1,55 @@
+/* @jsxImportSource vue */
+
+import { defineComponent, ref } from "vue";
+import "@sv/elements/blur";
+import { Button } from "./Button";
+import { Icon } from "./Icon";
+
+export const Lightbox = defineComponent((_, { slots }) => {
+  const open = ref(false);
+
+  return () => (
+    <div>
+      <Button
+        onClick={() => {
+          open.value = true;
+        }}
+      >
+        Open
+      </Button>
+
+      {/* should eventually use native <dialog /> */}
+      <a-blur
+        enabled={open.value || undefined}
+        class={[
+          "group/dialog fixed top-0 left-0 z-50 block h-full w-full opacity-0 transition-all",
+          "[&[enabled]]:bg-[#33333333] [&[enabled]]:opacity-100 [&[enabled]]:backdrop-blur-md",
+        ]}
+        onBlur={() => {
+          open.value = false;
+        }}
+      >
+        <div
+          class={[
+            "-translate-x-1/2 -translate-y-1/2 fixed top-1/2 left-1/2 min-w-[400px] transition-all",
+            "scale-95 group-[&[enabled]]/dialog:block group-[&[enabled]]/dialog:scale-100",
+          ]}
+        >
+          {slots.default?.()}
+        </div>
+
+        <div class="absolute top-20 right-20 z-50 text-2xl">
+          <Button
+            label="close"
+            variant="ghost"
+            onClick={() => {
+              open.value = false;
+            }}
+          >
+            <Icon name="close" />
+          </Button>
+        </div>
+      </a-blur>
+    </div>
+  );
+});
