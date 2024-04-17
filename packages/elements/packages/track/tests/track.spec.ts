21d098c237d20d785ebed2b515e68343c5b35d0a
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-04-17 21:07:29 +0200

    Try to add test for accessibility

--- packages/elements/packages/track/tests/track.spec.ts
+++ packages/elements/packages/track/tests/track.spec.ts
@@ -2,6 +2,27 @@ import { test, expect } from "bun:test";
 
 const NODE_NAME = "a-track";
 
+async function trackWithChildren() {
+  const { Track } = await import("@sv/elements/track");
+
+  const track = new Track();
+  track.style.width = "800px";
+  track.style.height = "200px";
+  for (let i = 0; i < 10; i++) {
+    const div = document.createElement("div");
+    div.style.flex = "none";
+    div.style.height = "200px";
+    div.style.height = "200px";
+    track.append(div);
+  }
+
+  // TODO: IntersectionObserver not defined
+  // TODO  event.eventPhase = EventPhaseEnum.atTarget; TypeError: Attempted to assign to readonly property.
+  // document.body.append(track);
+
+  return track;
+}
+
 test("import track element", async () => {
   const { Track } = await import("@sv/elements/track");
   expect(Track).toBeDefined();
@@ -20,19 +41,24 @@ test("import track element", async () => {
 });
 
 test("check default traits", async () => {
-  const { Track } = await import("@sv/elements/track");
-
-  const track = new Track();
+  const track = await trackWithChildren();
   expect(track.findTrait("pointer")).toBeDefined();
 });
 
 test("check snap trait", async () => {
-  const { Track } = await import("@sv/elements/track");
-
-  const track = new Track();
+  const track = await trackWithChildren();
   track.snap = true;
   // @ts-ignore: simulate update
   track.updated();
 
   expect(track.findTrait("snap")).toBeDefined();
 });
+
+// TODO: *see above*
+// test("tab navigation", async () => {
+//   const track = await trackWithChildren();
+//   track.children[8]?.focus();
+//   track.updated();
+
+//   console.log(track.position);
+// });
