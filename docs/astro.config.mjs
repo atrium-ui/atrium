96e1914de5192fb8693f03a0b44a1a16d48a6cbb
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-05-22 14:01:20 +0200

    Fix check

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -29,7 +29,7 @@ export default defineConfig({
       jsx: true,
       template: {
         compilerOptions: {
-          isCustomElement: tag => tag.includes("-"),
+          isCustomElement: (tag) => tag.includes("-"),
         },
       },
     }),
