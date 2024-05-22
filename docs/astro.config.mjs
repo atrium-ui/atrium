fa4a05ad7b3d81f17e93460459214f95f59ed955
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-05-22 13:57:35 +0200

    Add design page to navigation

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -29,7 +29,7 @@ export default defineConfig({
       jsx: true,
       template: {
         compilerOptions: {
-          isCustomElement: (tag) => tag.includes("-"),
+          isCustomElement: tag => tag.includes("-"),
         },
       },
     }),
@@ -61,6 +61,10 @@ export default defineConfig({
         {
           label: "Usage",
           link: "/usage",
+        },
+        {
+          label: "Design",
+          link: "/design",
         },
         {
           label: "Component Templates",
