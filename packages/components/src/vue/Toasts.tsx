02ab816ee46d69801b886b9f05fda0884f853f03
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-06-18 18:12:46 +0200

    fix(components): add toast function to component

--- packages/components/src/vue/Toasts.tsx
+++ packages/components/src/vue/Toasts.tsx
@@ -1,7 +1,43 @@
 /* @jsxImportSource vue */
 import "@sv/elements/portal";
+import { Toast, ToastFeed } from "@sv/elements/toast";
 import { Button } from "./Button";
-import { toast } from "@sv/elements/toast";
+
+const TOAST_TYPE = {
+  default: "default",
+  error: "error",
+  transparent: "transparent",
+};
+
+export interface ToastOptions {
+  id?: string;
+  message?: string;
+  time?: number;
+  variant?: keyof typeof TOAST_TYPE;
+}
+
+const toasts: Map<string, Toast> = new Map();
+
+export function toast(options: ToastOptions, toastType: typeof Toast = Toast) {
+  if (options.id && toasts.has(options.id)) {
+    // kill existing toast with same id
+    const toast = toasts.get(options.id);
+    toast?.kill();
+    toasts.delete(options.id);
+  }
+
+  const toast = new toastType(options);
+  toast.className = `variant-${options.variant || TOAST_TYPE.default}`;
+  const feed = ToastFeed.getInstance();
+  const ele = document.createElement("div");
+  ele.style.margin = "4px 0";
+  ele.append(toast);
+  feed?.append(ele);
+
+  if (options.id) toasts.set(options.id, toast);
+
+  return toast;
+}
 
 export function ToastDemo() {
   return (
