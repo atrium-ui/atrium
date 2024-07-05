9e5cdeca8023d5d348c8a9c82834bab95af35b49
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-07-05 16:15:44 +0200

    fix(elements): a-scroll: add a histroy strategy to store scroll position per histroy state

--- packages/elements/packages/scroll/src/index.ts
+++ packages/elements/packages/scroll/src/index.ts
@@ -1,14 +1,52 @@
 // Awsome: https://begin.com/blog/posts/2023-01-12-restoring-scroll-position-for-server-rendered-sites
 
+const historyStorage = {
+  setItem(key: string, value: string) {
+    history.replaceState(
+      {
+        [`a-scroll-${key}`]: value,
+      },
+      "",
+    );
+  },
+  getItem(key: string) {
+    return window.history.state?.[`a-scroll-${key}`];
+  },
+};
+
+type Strategy = "session" | "history";
+
 class ScrollElement extends HTMLElement {
+  fallbackName() {
+    return `${this.className}-${this.parentElement?.className}`.replace(" ", "-");
+  }
+
+  get name() {
+    return this.getAttribute("name") || this.fallbackName();
+  }
+
+  get strategy(): Strategy {
+    return (this.getAttribute("strategy") as Strategy) || "session";
+  }
+
   connectedCallback() {
-    const top = sessionStorage.getItem("docs-sidebar-scroll");
-    if (top !== null) {
-      this.scrollTop = Number.parseInt(top, 10);
+    const storage =
+      this.strategy === "session"
+        ? sessionStorage
+        : this.strategy === "history"
+          ? historyStorage
+          : undefined;
+
+    if (storage) {
+      const top = storage.getItem(this.name);
+      if (top !== null) {
+        this.scrollTop = Number.parseInt(top, 10);
+      }
+
+      window.addEventListener("beforeunload", () => {
+        storage.setItem(this.name, this.scrollTop.toString());
+      });
     }
-    window.addEventListener("beforeunload", () => {
-      sessionStorage.setItem("docs-sidebar-scroll", this.scrollTop.toString());
-    });
   }
 }
 
