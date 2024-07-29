f87c92d16ba5174dd7c55fea990a0371ff6bbf24
Alessandro Scalisi <a.scalisi@s-v.de>
2024-07-29 11:38:01 +0200

    feat(elements): a-toast-feed: add nice stack animation

--- packages/components/src/vue/Toasts.tsx
+++ packages/components/src/vue/Toasts.tsx
@@ -2,7 +2,6 @@
 import "@sv/elements/portal";
 import "@sv/elements/toast";
 import { Toast, ToastFeed } from "@sv/elements/toast";
-import { Button } from "./Button";
 
 const TOAST_TYPE = {
   default: "default",
@@ -30,33 +29,17 @@ export function toast(options: ToastOptions, toastType: typeof Toast = Toast) {
   const toast = new toastType(options);
   toast.className = `variant-${options.variant || TOAST_TYPE.default}`;
   const feed = ToastFeed.getInstance();
-  const ele = document.createElement("div");
-  ele.style.margin = "4px 0";
-  ele.append(toast);
-  feed?.append(ele);
+  feed?.append(toast);
 
   if (options.id) toasts.set(options.id, toast);
 
   return toast;
 }
 
-export function ToastDemo() {
+export function Toasts() {
   return (
-    <div>
-      <Button
-        onClick={() => {
-          toast({
-            message: "Lorem ipsum is simply dummy text",
-            time: 3000,
-          });
-        }}
-      >
-        Show Toast
-      </Button>
-
-      <a-portal>
-        <a-toast-feed class="fixed right-12 bottom-12 text-base" />
-      </a-portal>
-    </div>
+    <a-portal>
+      <a-toast-feed class="fixed right-12 bottom-12 text-base" />
+    </a-portal>
   );
 }
