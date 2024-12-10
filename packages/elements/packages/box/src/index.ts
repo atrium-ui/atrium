5407a3b99d997262feb0873bc39540f98421d4ec
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-12-10 13:44:13 +0100

    cleanup box code

--- packages/elements/packages/box/src/index.ts
+++ packages/elements/packages/box/src/index.ts
@@ -1,107 +1,15 @@
-import { LitElement, html, css } from "lit";
+/**
+ * @license
+ * Copyright (c) 2024 Atrium Contributors
+ * SPDX-License-Identifier: MIT
+ */
 
-const boxes = new Set<BoxElement>();
+import { BoxElement } from "./Box";
 
-let eventCount = 0;
-let eventCountResolved = 0;
-
-const resizeObserver = new ResizeObserver((entries) => {
-  eventCount++;
-  console.debug("resize");
-
-  for (const entry of entries) {
-    if (entry.target instanceof BoxElement) {
-      entry.target.resize();
-    }
-  }
-});
-
-class BoxElement extends LitElement {
-  private baseDebounceTime = 25;
-  private maxDebounceTime = 150;
-  private debounceTime = this.baseDebounceTime;
-  private timer: Timer | undefined;
-  private timerResolved = false;
-
-  private get ele() {
-    return this.shadowRoot?.children[0] as HTMLElement;
-  }
-
-  private lock() {
-    if (!this.timerResolved) return;
-
-    this.ele.style.width = `${this.offsetWidth}px`;
-    this.ele.style.height = `${this.offsetHeight}px`;
-    this.ele.style.backgroundColor = "red";
-    this.style.overflow = "hidden";
-  }
-
-  private unlock() {
-    this.ele.style.width = "";
-    this.ele.style.height = "";
-    this.style.overflow = "";
-    this.ele.style.backgroundColor = "";
-    this.debounceTime = this.baseDebounceTime;
-  }
-
-  public resize() {
-    this.lock();
-
-    if (this.timer) {
-      this.debounceTime = Math.min(this.debounceTime + 2, this.maxDebounceTime);
-
-      clearTimeout(this.timer);
-      this.timerResolved = false;
-    } else {
-      this.onResize();
-      this.timerResolved = true;
-    }
-
-    const timer = setTimeout(() => {
-      this.timer = undefined;
-      if (!this.timerResolved) {
-        this.onResize();
-      }
-    }, this.debounceTime);
-
-    this.timer = timer;
-  }
-
-  private onResize() {
-    this.unlock();
-
-    eventCountResolved++;
-
-    const ev = new CustomEvent("resize", { cancelable: true });
-    this.dispatchEvent(ev);
-
-    console.debug(
-      100 - Math.floor((eventCountResolved / eventCount) * 100),
-      "% events saved",
-    );
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    boxes.add(this);
-    resizeObserver.observe(this);
-  }
-
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    boxes.delete(this);
-    resizeObserver.unobserve(this);
-  }
-
-  static styles = css`
-    :host {
-      display: block;
-    }
-  `;
-
-  render() {
-    return html`<div><slot></slot></div>`;
-  }
+try {
+  customElements.define("a-box", BoxElement);
+} catch (err) {
+  console.warn("a-box already defined");
 }
 
-customElements.define("a-box", BoxElement);
+export { BoxElement };
