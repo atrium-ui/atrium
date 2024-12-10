0b42d5d61e0a9068d16bd00d99aed216ea5d20bf
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-12-10 19:03:04 +0100

    fix: Typo

--- packages/elements/packages/box/src/Box.ts
+++ packages/elements/packages/box/src/Box.ts
@@ -79,7 +79,9 @@ export class BoxElement extends LitElement {
       display: block;
     }
     div {
-      display: inehrit;
+      display: inherit;
+      width: 100%;
+      height: 100%;
     }
   `;
 
