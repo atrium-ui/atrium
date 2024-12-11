e4ad67f7edc6f67f343625b4f9ef2a4c064f0be2
Tim Havlicek <t.havlicek@s-v.de>
2024-12-11 19:44:07 +0100

    feat(elements): Expandable open on anchor link (svp/atrium!23)

--- packages/elements/packages/expandable/src/Expandable.ts
+++ packages/elements/packages/expandable/src/Expandable.ts
@@ -7,6 +7,14 @@ declare global {
   }
 }
 
+let windowLoaded = false;
+
+if (typeof window !== "undefined") {
+  window.addEventListener("load", () => {
+    windowLoaded = true;
+  });
+}
+
 let accordionIncrement = 0;
 
 /**
@@ -103,12 +111,13 @@ export class Expandable extends LitElement {
     const ev = new CustomEvent("change", { bubbles: true, cancelable: true });
     this.dispatchEvent(ev);
 
-    if (!ev.defaultPrevented)
+    if (!ev.defaultPrevented && windowLoaded) {
       this.scrollIntoView({
         behavior: "smooth",
         block: "nearest",
         inline: "nearest",
       });
+    }
   }
 
   private get content() {
@@ -146,6 +155,29 @@ export class Expandable extends LitElement {
 
   private onClick(e: Event) {
     if (this.trigger?.contains(e.target as HTMLElement)) this.toggle();
+  }
+
+  private onDeeplink = () => {
+    if (!this.opened && location.hash) {
+      const deeplink = this.querySelector(location.hash) as HTMLElement;
+      if (deeplink) {
+        this.open();
+      }
+    }
+  };
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.onDeeplink();
+
+    window.addEventListener("hashchange", this.onDeeplink);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    window.removeEventListener("hashchange", this.onDeeplink);
   }
 
   private renderToggle() {
