ff156fa77bc43642725932d6cef200d5a03e265c
Tim Havlicek <tim.h4vlicek@gmail.com>
2025-01-27 17:07:02 +0100

    chore: Remove unmaintained packages

--- packages/html-to-custom-tag-replacement/test/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "bun:test";
-
-describe("html-to-custom-tag-replacement import", () => {
-  it("import module", async () => {
-    const out = await import(require.resolve("@sv/html-to-custom-tag-replacement"));
-    expect(out.default.name).toBe("HtmlToCustomTagReplacement");
-  });
-});
