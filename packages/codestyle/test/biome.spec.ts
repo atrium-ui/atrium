27cde8d0126fa353358badece880857a25d4ffc8
Tim Havlicek <tim.h4vlicek@gmail.com>
2025-02-01 17:02:47 +0100

    chore: Remove codestyles

--- packages/codestyle/test/biome.spec.ts
+++
@@ -1,9 +0,0 @@
-import { $ } from "bun";
-import { expect, test } from "bun:test";
-
-// bad test
-// test("valid biome configuration", async () => {
-//   // check runs in root of this repo, since it extends this package.
-//   const out = await $`bun x biome check .`;
-//   expect(out.exitCode).toBe(0);
-// });
