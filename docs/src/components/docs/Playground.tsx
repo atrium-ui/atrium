73ad14068629e733f32697e9a5911f79b26b7996
Tim Havlicek <t.havlicek@s-v.de>
2025-03-30 21:34:28 +0200

    docs: AI poayground (svp/atrium!39)

--- docs/src/components/docs/Playground.tsx
+++ docs/src/components/docs/Playground.tsx
@@ -13,8 +13,29 @@ import tsEditorWorker from "monaco-editor/esm/vs/language/typescript/ts.worker.j
 import exampleCodeHtml from "./playground/Examplecode.html.txt?raw";
 import exampleCodeTsx from "./playground/Examplecode.tsx.txt?raw";
 import { Toast } from "@sv/elements/toast";
+import { createSignal, onMount } from "solid-js";
+import { Blur } from "@sv/elements/blur";
+import { css, html } from "lit";
+
+const componentsImports = import.meta.glob(
+  "./../../../../packages/components/src/vue/*.tsx",
+  {
+    query: "?raw",
+  },
+);
 
 const SHARE_SERVICE_URL = "https://lkydy-sharing.deno.dev/";
+
+const files = {
+  "index.html": {
+    default: exampleCodeHtml,
+    model: null,
+  },
+  "index.tsx": {
+    default: exampleCodeTsx,
+    model: null,
+  },
+} as Record<string, { default: string; model: any }>;
 
 globalThis.MonacoEnvironment = {
   getWorkerUrl: (moduleId, label) => {
@@ -25,24 +46,7 @@ globalThis.MonacoEnvironment = {
   },
 };
 
-const componentsImports = import.meta.glob(
-  "./../../../../packages/components/src/vue/*.tsx",
-  {
-    query: "?raw",
-  },
-);
-
-const allComponentsCode = Object.entries(componentsImports).reduce(
-  (acc, [key, value]) => {
-    const name = key.split("/").pop()?.replace(".tsx", "");
-    if (!name) return acc;
-    acc[name] = value;
-    return acc;
-  },
-  {},
-);
-
-async function transform(code: string) {
+async function transformCode(code: string) {
   return await esbuild.transform(code, {
     sourcemap: false,
     minify: false,
@@ -54,137 +58,120 @@ async function transform(code: string) {
   });
 }
 
-let iframe: HTMLIFrameElement;
+class CustomToast extends Toast {
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        position: relative;
+        transition: opacity .5s ease, height 0.5s ease, margin 0.5s ease, transform 0.5s ease;
+        min-width: 220px;
+        max-width: 500px;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+      }
+      div {
+        padding: 0.25rem 0;
+      }
+    `;
+  }
+
+  protected render() {
+    return html`<slot><div><slot>${this.message}</slot></div></slot>`;
+  }
+}
+
+customElements.define("custom-toast", CustomToast);
 
 async function init() {
-  iframe = document.createElement("iframe");
-  iframe.src = "/atrium/story";
-
   await import("@atrium-ui/layout");
   await esbuild.initialize({ wasmURL: esbuildUrl });
-  const monaco: typeof import("monaco-editor") = await import(
-    "monaco-editor/esm/vs/editor/editor.main.js"
-  );
 
-  const files = {
-    "index.html": {
-      default: exampleCodeHtml,
-      model: null,
-    },
-    "index.tsx": {
-      default: exampleCodeTsx,
-      model: null,
-    },
-  } as Record<string, { default: string; model: any }>;
-
-  const query = location.search.slice(1);
-  if (query) {
-    const remotePreset = await fetch(`${SHARE_SERVICE_URL}${query}`).then((response) =>
-      response.text(),
-    );
-    files["index.tsx"].default = remotePreset;
-  }
-
-  async function pushCode(iframe: HTMLIFrameElement, _files: typeof files) {
-    iframe.contentWindow.location.reload();
-
-    await new Promise((resolve) => {
-      iframe.onload = resolve;
-    });
-
-    const script = document.createElement("script");
-    script.type = "module";
-    script.textContent = `import { h } from "vue";\n${(await transform(_files["index.tsx"]?.model.getValue())).code}`;
-    console.debug(script.textContent);
-
-    const root = iframe.contentWindow.document.querySelector("#root");
-    if (root) {
-      root.innerHTML = _files["index.html"]?.model.getValue();
-      iframe.contentWindow.document.body.appendChild(script);
-    }
-  }
-
-  async function share() {
-    try {
-      const value = files["index.tsx"]?.model.getValue();
-      const uid = await fetch(SHARE_SERVICE_URL, {
-        method: "POST",
-        body: value,
-      }).then((response) => response.text());
-
-      const url = `${location.origin}${location.pathname}?${uid}`;
-      const toast = new Toast({
-        message: url,
-        onClick: () => {
-          navigator.clipboard.writeText(url);
-        },
-      });
-      toastfeed.append(toast);
-    } catch (error) {
-      const toast = new Toast({
-        message: error.message,
-        onClick: () => {
-          navigator.clipboard.writeText(url);
-        },
-      });
-      toastfeed.append(toast);
-      console.error(error);
-    }
-  }
-
-  function initCommandMenu() {
-    const menu = document.querySelector(".commandmenu-wrapper");
-    const menuList = document.querySelector(".commandmenu");
-    window.addEventListener("keydown", (event) => {
-      if (event.key === "p" && event.metaKey) {
-        menu?.enable();
-        event.preventDefault();
-        event.stopPropagation();
+  customElements.define(
+    "command-menu",
+    class CommandMenu extends Blur {
+      constructor() {
+        super();
+        const root = this.attachShadow({ mode: "open" });
+        root.innerHTML = `
+          <slot></slot>
+        `;
       }
-    });
-    menuList?.addEventListener("change", async (event) => {
-      const value = menuList.getValueOfOption(event.option);
-      menu?.disable();
 
-      // copy component code
-      const module = await allComponentsCode[value]();
-      navigator.clipboard.writeText(module.default);
-    });
+      connectedCallback() {
+        super.connectedCallback();
 
-    const input = document.querySelector("input");
-    const list = document.querySelector(".commandmenu-list");
+        this.innerHTML = `
+          <a-list class="commandmenu">
+            <input type="text" placeholder="Search..." />
+            <div class="commandmenu-list" />
+          </a-list>
+        `;
 
-    if (input && list) {
-      const renderList = () => {
-        const filter = input?.value.toLowerCase();
-        list.innerHTML = "";
+        const allComponentsCode = Object.entries(componentsImports).reduce(
+          (acc, [key, value]) => {
+            const name = key.split("/").pop()?.replace(".tsx", "");
+            if (!name) return acc;
+            acc[name] = value;
+            return acc;
+          },
+          {},
+        );
 
-        for (const item of Object.keys(allComponentsCode)) {
-          if (!item.toLowerCase().match(filter)) continue;
+        const menuList = this.querySelector(".commandmenu");
+        window.addEventListener("keydown", (event) => {
+          if (event.key === "p" && event.metaKey) {
+            this.enable();
+            event.preventDefault();
+            event.stopPropagation();
+          }
+        });
+        menuList?.addEventListener("change", async (event) => {
+          const value = menuList.getValueOfOption(event.option);
+          this.disable();
 
-          const itemElement = document.createElement("a-list-item");
-          itemElement.setAttribute("value", item);
-          itemElement.innerHTML = item;
-          list.appendChild(itemElement);
+          // copy component code
+          const module = await allComponentsCode[value]();
+          navigator.clipboard.writeText(module.default);
+        });
+
+        const input = this.querySelector("input");
+        const list = this.querySelector(".commandmenu-list");
+
+        if (input && list) {
+          const renderList = () => {
+            const filter = input?.value.toLowerCase();
+            list.innerHTML = "";
+
+            for (const item of Object.keys(allComponentsCode)) {
+              if (!item.toLowerCase().match(filter)) continue;
+
+              const itemElement = document.createElement("a-list-item");
+              itemElement.setAttribute("value", item);
+              itemElement.innerHTML = item;
+              list.appendChild(itemElement);
+            }
+          };
+
+          input?.addEventListener("input", renderList);
+
+          renderList();
         }
-      };
-
-      input?.addEventListener("input", renderList);
-
-      renderList();
-    }
-  }
-
-  initCommandMenu();
+      }
+    },
+  );
 
   customElements.define(
     "monaco-editor",
-    class CodeViewMonaco extends HTMLElement {
+    class CodeViewMonaco extends (HTMLElement || {}) {
       _monacoEditor;
       _model;
 
-      constructor() {
-        super();
+      async connectedCallback() {
+        // TODO: styles are gone on page change
+        const monaco: typeof import("monaco-editor") = await import(
+          "monaco-editor/esm/vs/editor/editor.main.js"
+        );
 
         this._monacoEditor = monaco.editor.create(this, {
           automaticLayout: true,
@@ -214,14 +201,14 @@ async function init() {
         this._monacoEditor.setModel(model);
 
         model.onDidChangeContent((event) => {
-          pushCode(iframe, files);
+          this.dispatchEvent(new CustomEvent("change"));
         });
 
         this._model = model;
 
         this.load();
 
-        pushCode(iframe, files);
+        this.dispatchEvent(new CustomEvent("change"));
       }
 
       async load() {
@@ -231,55 +218,204 @@ async function init() {
       }
     },
   );
+}
 
-  return {
-    share,
-  };
+if (typeof window !== "undefined") {
+  init();
 }
 
 export function Playground() {
-  let share: () => Promise<string>;
+  const [loading, setLoading] = createSignal(false);
 
-  init().then((exp) => {
-    share = exp.share;
+  const iframe = document.createElement("iframe");
+  iframe.src = "/atrium/story";
+
+  async function loadFiles() {
+    const query = location.search.slice(1);
+    if (query) {
+      const remotePreset = await fetch(`${SHARE_SERVICE_URL}${query}`).then((response) =>
+        response.text(),
+      );
+      files["index.tsx"].default = remotePreset;
+    }
+  }
+
+  async function pushCode() {
+    setLoading(true);
+    iframe.contentWindow.location.reload();
+
+    await new Promise((resolve) => {
+      iframe.onload = () => {
+        setLoading(false);
+        resolve();
+      };
+    });
+
+    const script = document.createElement("script");
+    script.type = "module";
+    script.textContent = `import { h } from "vue";\n${(await transformCode(files["index.tsx"]?.model.getValue())).code}`;
+    console.debug(script.textContent);
+
+    const root = iframe.contentWindow.document.querySelector("#root");
+    if (root) {
+      root.innerHTML = files["index.html"]?.model.getValue();
+      iframe.contentWindow.document.body.appendChild(script);
+    }
+  }
+
+  function showToast(str: string) {
+    const toast = new CustomToast({
+      message: str,
+      onClick: () => {
+        toast.kill();
+      },
+    });
+    toastfeed.append(toast);
+    return toast;
+  }
+
+  async function share() {
+    try {
+      const value = files["index.tsx"]?.model.getValue();
+      const uid = await fetch(SHARE_SERVICE_URL, {
+        method: "POST",
+        body: value,
+      }).then((response) => response.text());
+
+      const url = `${location.origin}${location.pathname}?${uid}`;
+      toastfeed.append(
+        new CustomToast({
+          message: url,
+          onClick: () => {
+            navigator.clipboard.writeText(url);
+            showToast("Copied!");
+          },
+        }),
+      );
+    } catch (error) {
+      console.error(error);
+      showToast(error.message);
+    }
+  }
+
+  const GEMINI_SERVICE_URL = new URLSearchParams(location.search).get("gemini");
+
+  async function gemini() {
+    const clipboardContents = await navigator.clipboard.read();
+
+    const toast = showToast("Pasting image...");
+
+    try {
+      for (const item of clipboardContents) {
+        if (item.types.includes("image/png")) {
+          const blob = await item.getType("image/png");
+          const formData = new FormData();
+          formData.append("image", blob);
+
+          toast.innerText = "Waiting for response...";
+
+          console.info(...formData);
+
+          const response = await fetch(GEMINI_SERVICE_URL, {
+            method: "POST",
+            body: formData,
+          }).then((response) => response.text());
+
+          console.info(response);
+
+          await navigator.clipboard.writeText(response);
+
+          toast.innerText = "Copied response to clipboard";
+
+          return;
+        }
+      }
+    } catch (error) {
+      toast.kill();
+      console.error(error);
+      showToast(error.message);
+      return;
+    }
+
+    showToast("No image to paste");
+  }
+
+  onMount(async () => {
+    await loadFiles();
   });
 
   return (
     <div class={["relative", "not-content"].join(" ")}>
-      <a-layout class="z-0">
-        <a-layout-column>
-          <a-layout-group tabs>
-            <monaco-editor data-file="index.html" tab="index.html" />
-            <monaco-editor data-file="index.tsx" tab="index.tsx" />
-          </a-layout-group>
-        </a-layout-column>
-        <a-layout-column>
-          <a-layout-group tabs>
-            <div tab="Preview">{iframe}</div>
-          </a-layout-group>
-        </a-layout-column>
-      </a-layout>
+      <div class="flex">
+        <div class="flex flex-col gap-2 p-2">
+          <button
+            type="button"
+            class="cursor-pointer rounded bg-blue-500 p-3 font-bold text-white hover:bg-blue-700"
+            onClick={() => share()}
+            title="share"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 256 256"
+              class="h-[1em] w-[1em]"
+            >
+              <title>Share</title>
+              <path
+                fill="currentColor"
+                d="M229.66,109.66l-48,48a8,8,0,0,1-11.32-11.32L204.69,112H165a88,88,0,0,0-85.23,66,8,8,0,0,1-15.5-4A103.94,103.94,0,0,1,165,96h39.71L170.34,61.66a8,8,0,0,1,11.32-11.32l48,48A8,8,0,0,1,229.66,109.66ZM192,208H40V88a8,8,0,0,0-16,0V216a8,8,0,0,0,8,8H192a8,8,0,0,0,0-16Z"
+              />
+            </svg>
+          </button>
 
-      <div class="-top-16 absolute right-4 z-50">
-        <button
-          type="button"
-          class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-          onClick={() => {
-            share();
-          }}
-        >
-          Share
-        </button>
+          {GEMINI_SERVICE_URL ? (
+            <button
+              type="button"
+              class="cursor-pointer rounded bg-blue-500 p-3 font-bold text-white hover:bg-blue-700"
+              onClick={() => gemini()}
+              title="gemini"
+            >
+              <svg
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-[1em] w-[1em]"
+              >
+                <title>Gemini</title>
+                <path
+                  d="M12.0002 23C12.0002 21.4783 11.7068 20.0483 11.1199 18.71C10.5519 17.3717 9.7725 16.2075 8.7826 15.2175C7.7927 14.2275 6.62836 13.4483 5.29001 12.88C3.95167 12.2933 2.5218 12 1 12C2.5218 12 3.95167 11.7159 5.29001 11.1475C6.62836 10.5609 7.7927 9.7725 8.7826 8.78252C9.7725 7.7925 10.5519 6.62834 11.1199 5.29C11.7068 3.95167 12.0002 2.52167 12.0002 1C12.0002 2.52167 12.2842 3.95167 12.8526 5.29C13.4391 6.62834 14.2275 7.7925 15.2174 8.78252C16.2077 9.7725 17.3716 10.5609 18.71 11.1475C20.0483 11.7159 21.4786 12 23 12C21.4786 12 20.0483 12.2933 18.71 12.88C17.3716 13.4483 16.2077 14.2275 15.2174 15.2175C14.2275 16.2075 13.4391 17.3717 12.8526 18.71C12.2842 20.0483 12.0002 21.4783 12.0002 23Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <a-layout class="z-0">
+          <a-layout-column>
+            <a-layout-group tabs>
+              <monaco-editor
+                data-file="index.html"
+                tab="index.html"
+                onChange={pushCode}
+              />
+              <monaco-editor data-file="index.tsx" tab="index.tsx" onChange={pushCode} />
+            </a-layout-group>
+          </a-layout-column>
+          <a-layout-column>
+            <a-layout-group tabs>
+              <div tab="Preview" class="flex h-full flex-col">
+                {loading() ? <div>Loading...</div> : null}
+                <div class="flex-1">{iframe}</div>
+              </div>
+            </a-layout-group>
+          </a-layout-column>
+        </a-layout>
       </div>
 
-      <a-blur class="commandmenu-wrapper">
-        <a-list class="commandmenu">
-          <input type="text" placeholder="Search..." />
-          <div class="commandmenu-list" />
-        </a-list>
-      </a-blur>
+      <command-menu />
 
-      <a-toast-feed id="toastfeed" class="absolute right-4 bottom-4 z-50" />
+      <a-toast-feed id="toastfeed" class="-translate-x-1/2 fixed top-16 left-1/2 z-100" />
     </div>
   );
 }
