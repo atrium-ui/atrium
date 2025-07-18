7581cefb1b65e3e89e1c68baa764ca65c8c2268f
Tim Havlicek <t.havlicek@s-v.de>
2025-07-18 04:58:53 +0200

    docs: Cleanup

--- docs/src/content/components/popover/tooltip.stories.tsx
+++ docs/src/content/components/popover/tooltip.stories.tsx
@@ -1,5 +1,4 @@
 /* @jsxImportSource vue */
-import { Button } from "@components/src/vue/Button.jsx";
 import type { Story } from "../../../components/stories/stories.js";
 import { Tooltip as ToolTip } from "@components/src/vue/Tooltip";
 
@@ -16,15 +15,11 @@ export default {
   render: (args) => {
     return (
       <div class="flex min-h-[200px] max-w-full items-center justify-center">
-        <Button>Button</Button>
-
         <ToolTip label="Hover">
           <div class="p-3">
             <p>Some Content</p>
           </div>
         </ToolTip>
-
-        <Button>Button</Button>
       </div>
     );
   },
