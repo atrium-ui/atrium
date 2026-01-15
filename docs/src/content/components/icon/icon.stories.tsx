6ced64b16efed3391e1b433f9a38d7ee2d906e41
Tim Havlicek <t.havlicek@s-v.de>
2026-01-15 16:49:44 +0100

    feat(components): Update icon component to use the icons package

---
+++ docs/src/content/components/icon/icon.stories.tsx
@@ -0,0 +1,19 @@
+/* @jsxImportSource vue */
+
+import { Icon } from "@components/src/vue/Icon";
+import type { Story } from "../../../components/stories/stories.js";
+import * as IconSet from "@sv/icons";
+
+export default {
+  tags: ["public"],
+  args: {},
+  argTypes: {},
+} satisfies Story;
+
+export const Default = {
+  render: () => (
+    <div class="flex gap-6 flex-wrap p-6 max-w-full" style="font-size: 2rem;">
+      {IconSet.icons.map(icon => <Icon name={icon} />)}
+    </div>
+  ),
+};
